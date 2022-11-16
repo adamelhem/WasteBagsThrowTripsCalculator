3224789d10c2605052620e52f294974e1b0fdbb5
Adam Melhem <Adam Melhem@DESKTOP-NVK9QMH>
2022-11-16 07:26:46 +0200

    set project structure

--- WasteBagsThrowTripsCalculator/Program.cs
+++ WasteBagsThrowTripsCalculator/Program.cs
@@ -1,9 +1,2 @@
-﻿Console.WriteLine("Please Enter the amount of bags, followed by each bag weight");
-var bagsAmount = int.Parse(Console.ReadLine());
-var inputs = new List<bag>();
-for (var i = bagsAmount ; i > 0; i--)
-{
-    inputs.Add(new bag { Id = i , weight = float.Parse(Console.ReadLine()) });
-}
-
-Console.WriteLine(new WasteBagsServer(inputs).CalculateTrips());
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
